07face57f3f0a111082acbd2e26f9a6e7959ce49
Neel Gonuguntla <neel.gonuguntla@gmail.com>
2025-02-19 14:39:38 +0530

    Update pattern.py

--- CS1280--PPDA/Lab4/pattern.py
+++ CS1280--PPDA/Lab4/pattern.py
@@ -1,5 +1,8 @@
 n=int(input("Enter number of lines in pattern : "))
+sp=n-1
 for i in range(1,n+1):
+      print(" "*sp,end="")
       for j in range(i):
             print("*",end=" ")
+      sp-=1
       print()
