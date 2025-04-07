30fab45030aa3eb289be937ffdf86f0b7370e4b9
Neel Gonuguntla <neel.gonuguntla@gmail.com>
2025-04-07 09:56:01 +0530

    Update act11.py

--- CS1280--PPDA/Labrecord/act11.py
+++ CS1280--PPDA/Labrecord/act11.py
@@ -1,9 +1,22 @@
 import pandas as pd
+
+#Using Normalizer
 df=pd.read_csv('3Salary_Data.csv')
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
 
-scaler=StandardScaler()
-scaled_data=scaler.fit_transform(df)
-scaled_df1=pd.DataFrame(scaled_data,columns=df.columns)
-scaled_df1
+normalizer=Normalizer()
+normalized_data=normalizer.fit_transform(df)
+normalized_df1=pd.DataFrame(normalized_data,columns=df.columns)
+normalized_df1
+
+#Using MinMaxScaler
+df=pd.read_csv('3Salary_Data.csv')
+
+from sklearn.preprocessing import MinMaxScaler
+
+minmaxscaler=MinMaxScaler()
+minmaxscaled_data=minmaxscaler.fit_transform(df)
+minmaxscaled_df1=pd.DataFrame(minmaxscaled_data,columns=df.columns)
+minmaxscaled_df1
+
